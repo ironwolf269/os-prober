f330aab4202dc3da2f81d08090b5fca2a252a322
ironwolf269 <88604362+ironwolf269@users.noreply.github.com>
2021-08-16 12:18:17 -0700

    Update grub_os-prober.sh

--- grub_os-prober.sh
+++ grub_os-prober.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This will add the lines # Check for other operating systems GRUB_DISABLE_OS_PROBER=false
 
-echo' 
+echo ' 
 # Check for other operating systems
 GRUB_DISABLE_OS_PROBER=false' | tee -a /etc/default/grub
